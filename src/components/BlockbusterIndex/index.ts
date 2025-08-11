87bbd3511410d2100f5fcbf67381d670cdadae41
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-08-10 19:13:26 -0700

    fix: fix build

--- src/components/BlockbusterIndex/index.ts
+++ src/components/BlockbusterIndex/index.ts
@@ -1,5 +1,4 @@
 export { default } from './BlockbusterIndex';
-export { default as Header } from './Header';
 export { default as SubHeader } from './SubHeader';
 export { default as VisualizationRouter } from './VisualizationRouter';
 export { SelectedRegionCharts } from '@components/Charts';
